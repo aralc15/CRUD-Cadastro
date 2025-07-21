03ed568cee3be8295cc384219b55689959684513
Ana Clara Araujo da Cruz <JT0099374@MATEUS.DC>
2025-07-21 11:15:52 -0300

    Initial commit

---
+++ Usuario-Cadastro/src/main/java/com/ACcompany/Usuario_Cadastro/UsuarioCadastroApplication.java
@@ -0,0 +1,13 @@
+package com.ACcompany.Usuario_Cadastro;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UsuarioCadastroApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(UsuarioCadastroApplication.class, args);
+	}
+
+}
---
+++ Usuario-Cadastro/src/test/java/com/ACcompany/Usuario_Cadastro/UsuarioCadastroApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ACcompany.Usuario_Cadastro;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UsuarioCadastroApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
